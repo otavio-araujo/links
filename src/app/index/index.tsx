aa7923e282419804a0f8da500a63bff699e68c04
Otávio Araújo <otavio_araujo@hotmail.com>
2024-09-29 03:30:25 -0300

    creates-index-header

---
+++ src/app/index/index.tsx
@@ -0,0 +1,18 @@
+import { Image, TouchableOpacity, View } from "react-native"
+import { styles } from "@/app/index/styles"
+import { MaterialIcons } from "@expo/vector-icons"
+import { colors } from "@/styles/colors"
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+        <TouchableOpacity>
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
---
+++ src/app/index/styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native"
+import { colors } from "@/styles/colors"
+import { MaterialIcons } from "@expo/vector-icons"
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 62,
+  },
+  title: {
+    color: colors.green[900],
+    fontSize: 22,
+  },
+  header: {
+    paddingHorizontal: 24,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+    marginBottom: 32,
+  },
+  logo: {
+    width: 32,
+    height: 38,
+  },
+})
