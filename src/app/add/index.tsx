e7c6afcdef15f407dbdbd83060b02224baf17f0b
Otávio Araújo <otavio_araujo@hotmail.com>
2024-09-29 21:24:06 -0300

    Recuperando Valor do Input - Nivel 04 - Estados de Navecação

---
+++ src/app/add/index.tsx
@@ -0,0 +1,33 @@
+import { View, Text, TouchableOpacity } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
+import { router } from "expo-router"
+
+import { styles } from "./styles"
+import { colors } from "@/styles/colors"
+
+import { Categories } from "@/components/categories"
+import { Input } from "@/components/input"
+import { Button } from "@/components/button"
+
+export default function Add() {
+  return (
+    <View style={styles.contaier}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.back()}>
+          <MaterialIcons name="arrow-back" size={32} color={colors.gray[200]} />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Novo</Text>
+      </View>
+
+      <Text style={styles.label}>Selecione uma categoria</Text>
+      <Categories />
+
+      <View style={styles.form}>
+        <Input placeholder="Nome" onChangeText={console.warn} />
+        <Input placeholder="Url" />
+        <Button title="Adicionar" />
+      </View>
+    </View>
+  )
+}
